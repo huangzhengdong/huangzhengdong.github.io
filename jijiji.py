ee9c233d550cacf006411a2f23ed5fca1917d0e7
huangzhengdong <1761503659@qq.com>
2022-10-15 23:23:51 +0800

    猜数字游戏

--- jijiji.py
+++ jijiji.py
@@ -1,17 +1,17 @@
-"""用python设计第一个游戏"""
+"""python小游戏"""
 import random
-answer=random.randint(1, 10)
+answer=random.randint(1,10)
 counts=3
 while counts>0:
-    temp=input("不妨猜一下小甲鱼现在心里想的是哪个数字")
+    temp=input("猜猜我心里想的数字")
     guess=int(temp)
     if guess==answer:
-        print("恭喜你猜对了")
+        print("你猜对了")
         break
     else :
         if guess>answer:
             print("大了")
-        else:
+        else :
             print("小了")
     counts=counts-1
-print("游戏结束，不玩了")
+print("游戏结束")
