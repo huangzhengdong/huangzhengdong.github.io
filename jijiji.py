c1dc6e21dd1c244e632b5dfdc29c021118629bd5
huangzhengdong <1761503659@qq.com>
2022-10-15 23:08:10 +0800

    pythin游戏

--- jijiji.py
+++ jijiji.py
@@ -1,1 +1,17 @@
-print("hello world!")
+"""用python设计第一个游戏"""
+import random
+answer=random.randint(1, 10)
+counts=3
+while counts>0:
+    temp=input("不妨猜一下小甲鱼现在心里想的是哪个数字")
+    guess=int(temp)
+    if guess==answer:
+        print("恭喜你猜对了")
+        break
+    else :
+        if guess>answer:
+            print("大了")
+        else:
+            print("小了")
+    counts=counts-1
+print("游戏结束，不玩了")
